7bc2016d49e68b7ce6b6b91dbbadd860a502ba55
Katharyn Lynn Oggenfuss <koggenfuss@gmail.com>
2021-03-19 09:35:06 -0400

    Seeded DB and using DB to make API call

--- rest/main.js
+++ rest/main.js
@@ -9,84 +9,64 @@ const apiUrl = "https://www.googleapis.com/books/v1/volumes?q=";
 const app = express();
 const port = 3000;
 
-app.listen(port, () => {
-    console.log(`Listening at http://localhost:${port}`)
-});
+// app.listen(port, () => {
+//     console.log(`Listening at http://localhost:${port}`)
+// });
 
-  async function getFromAPI(){
-    let promises = []
-    for (let i = 1; i <= 151; i++){
-      let response = axios.get(`https://pokeapi.co/api/v2/pokemon/${i}`)
-      promises.push(response)
-    }
-    let responses = await Promise.all(promises)
-    return responses.map(response => response.data).map(data => ({
-      "id": data.id,
-      "name": data.name,
-      "types": data.types.map(type => type.type.name),
-      "img": data.sprites["other"]["official-artwork"]["front_default"]
-    }))
+  // async function getFromAPI(bookTitleAndAuthorAPIURL){
+  //   let promises = [];
+  //   for (let i = 1; i <= 5; i++){
+  //     let response = axios.get(bookTitleAndAuthorAPIURL);
+  //     promises.push(response);
+  //   }
+  //   let responses = await Promise.all(promises);
+  //   console.log(responses);
+    //return responses.map(response => response.data).map(data => ({
+
+  //}
  
-  }
-  getFromAPI();
+  // }
+  // getFromAPI();
 
 function loadDatabase(){
   let con = mysql.createConnection({
     host:"localhost",
     user: "root",
-    password: password,
+    password: "KillEric5050",
     database:"My_Library"
 });
-
-    for (let i = 1; i <= 1; i++) {
-      console.log(con.query("SELECT COUNT(*) FROM books"));
-      console.log(con.query(`SELECT title FROM books WHERE id = ${i}`));
-      console.log(con.query(`SELECT author FROM books WHERE id = ${i}`));
-      //getFromAPI(`${apiUrl}"${title}+inauthor:${author}`);
-    }
-  }
-  loadDatabase();
-
-//   //let name = con.query("SELECT name FROM books")
-//   let data = await getFromAPI(); 
-//   data.forEach(pokemon => {
-//     con.query(`INSERT INTO pokemon (id, name, img) VALUES (${pokemon.id}, "${pokemon.name}", "${pokemon.img}");`);
-//   })
-//   let uniqueTypes = new Set();
-//     data.forEach(pokemon => {
-//       pokemon.types.forEach(type => {
-//       uniqueTypes.add(type) 
-//     })
-//   }) 
-//   uniqueTypes = [...uniqueTypes]
-//   for(let i = 0; i < uniqueTypes.length; i++) {
-//     con.query(`INSERT INTO type (id, name) VALUES (${i+1}, "${uniqueTypes[i]}");`)
-//   }
-//   data.forEach(pokemon => {
-//     pokemon.types.forEach(type => {
-//       let typeId = uniqueTypes.indexOf(type) + 1;
-//       con.query(`INSERT INTO pokemon_type (pokemonId, typeId) VALUES (${pokemon.id}, ${typeId});`)
-//     })
-//   })
-//   console.log(data)
-//   con.end();
-// }
-//loadDatabase()
-
-// async function getPokemonsFromDatabase(){
-//   let con = mysql.createConnection({
-//         host:"localhost",
-//         user: "root",
-//         password: password,
-//         database:"pokedex"
-//     });
-
-//     let data = await new Promise((resolve, reject) => {
-//      con.query("SELECT * FROM pokemon", (err, result, fields) => {
-//       (err) ? reject(err) : resolve(result);
-//       });
-//     });
+    let title;
+    let author;
     
-//    con.end();
-//    return data;
-// }
+    con.connect (err => {
+      if (err) {
+        throw err;
+      }
+    for (let i = 1; i <= 5; i++) {
+      con.query(`SELECT title FROM books WHERE id = ${i}`, (err, results, fields) => {
+        if (err) {
+          throw err;
+        }
+        title = results[0].title;
+        console.log(title);
+      });
+      con.query(`SELECT author FROM books WHERE id = ${i}`, (err, results, fields) => {
+        if (err) {
+          throw err;
+        }
+        author = results[0].author;
+        console.log(author);
+        //getFromAPI
+        
+        console.log(`${apiUrl}"${title}"+inauthor:"${author}"`);
+    });
+    
+
+    
+  }
+  con.end();
+ });
+}
+
+loadDatabase();
+
